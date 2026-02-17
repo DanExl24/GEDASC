b32e10c67cfb4a6ed777891d0660634b8790e9c8
Jorge <penamottajorgealejandro@gmail.com>
2026-02-17 17:22:11 -0500

    SCRUM-9 Boton de ir atras terminado

---
+++ src/components/UI/ExitButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link :to="to">
+      <div class="bg-senaColor p-3 m-3 w-12 border rounded-md hover:scale-110 transition-transform duration-800">
+        <img class="" :src="ButtonImg" alt="">
+      </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+
+import back from '@/assets/Icons/Back.png'
+
+withDefaults(defineProps<{
+  to : string,
+  ButtonImg? : string
+}>(),{
+  ButtonImg : back
+})
+
+</script>
+<style>
+
+</style>
--- src/views/GeneralEntryView.vue
+++ src/views/GeneralEntryView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderView -header-title="INGRESO DE APRENDICES AL CENTRO DE FORMACION"/>
+  <ExitButton to="/"/>
 </template>
 <script setup lang="ts">
-
+import HeaderView from '@/layouts/HeaderView.vue';
+import ExitButton from '@/components/UI/ExitButton.vue';
 </script>
 <style>
 
